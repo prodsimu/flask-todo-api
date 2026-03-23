ff4fda6e0c0e28d0ddb272ac948c0449815a52de
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-23 19:20:10 -0300

    feat: implement Flask app factory pattern

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from .database import db
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    return app
