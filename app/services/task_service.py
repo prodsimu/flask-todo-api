aab0f68913f7e22453fbf29b1063964264a86a8a
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-30 10:10:43 -0300

    feat(services): create TaskService class with create_task method

---
+++ app/services/task_service.py
@@ -0,0 +1,47 @@
+from app.database.database import db
+from app.models import Project, Task, TaskPriority, TaskStatus
+
+
+class TaskService:
+
+    # CREATE
+
+    @staticmethod
+    def create_task(
+        project_id: int,
+        owner_id: int,
+        title: str,
+        description: str = None,
+        priority: str = TaskPriority.MEDIUM.value,
+    ):
+        project = db.session.get(Project, project_id)
+
+        if not project:
+            raise ValueError("Project not found.")
+
+        if project.owner_id != owner_id:
+            raise PermissionError("Access denied.")
+
+        if not title or len(title.strip()) == 0:
+            raise ValueError("Title is required.")
+
+        if len(title) > 100:
+            raise ValueError("Title must be at most 100 characters.")
+
+        if description and len(description) > 255:
+            raise ValueError("Description must be at most 255 characters.")
+
+        if priority not in [p.value for p in TaskPriority]:
+            raise ValueError("Invalid priority.")
+
+        task = Task(
+            title=title.strip(),
+            description=description,
+            priority=priority,
+            project_id=project_id,
+        )
+
+        db.session.add(task)
+        db.session.commit()
+
+        return task
